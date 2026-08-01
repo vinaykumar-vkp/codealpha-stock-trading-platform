d4ef82f351e192e660ba03f6d8827baa8f096ef6
Vinay Kumar Patel <vinaykumarpatel955@gmail.com>
2026-08-01 16:00:53 +0530

    Create Main.java
    Added Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.codealpha.stocktrading;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TradingService service = new TradingService();
+        service.start();
+
+    }
+}
